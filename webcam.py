9aeb2c82b17e84577d47ca89a34460efc152f116
Mineev <mineev.buryat@gmail.com>
2024-11-30 17:49:23 +0800

    web camera practics!

--- webcam.py
+++ webcam.py
@@ -1,16 +1,15 @@
 import cv2
+import numpy as np
 
 # Включаем первую камеру
 cap = cv2.VideoCapture(0)
-
-# "Прогреваем" камеру, чтобы снимок не был тёмным
-
-
-# Делаем снимок    
 ret, frame = cap.read()
-
-# Записываем в файл
-cv2.imwrite('cam.png', frame)   
-
+z = np.zeros((3,), dtype=np.uint8)
 # Отключаем камеру
 cap.release()
+print(type(frame), frame)
+print(len(frame), len(frame[0]), len(frame[0][0]))
+for line in frame:
+    for pixel in line:
+        pixel = np.append(pixel, z[0])
+        print(pixel, bytes(pixel))
